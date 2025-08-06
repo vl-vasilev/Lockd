6066c8175571f27d9a2ab86aaffbdd40ad135514
Vladimir Vasilev <vl.vasilev08@gmail.com>
2025-08-06 22:15:24 +0300

    made tabs and added Colors object for consistent UI

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,17 +1,22 @@
-import Foundation from '@expo/vector-icons/Foundation';
-import Octicons from '@expo/vector-icons/Octicons';
+import Colors from '@/constants/Colors';
+import { Octicons } from '@react-native-vector-icons/octicons';
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
     <Tabs screenOptions={{
-      tabBarActiveTintColor: "coral",
+      tabBarActiveTintColor: Colors.primary,
+      tabBarStyle: {
+        elevation: 0,
+        borderTopWidth: 1,
+        paddingTop: 8,
+      }
     }}>
       <Tabs.Screen name="index" options={{
         title: "Home",
         tabBarIcon: ({ color, focused }) => {
           return focused ? (
-            <Foundation name="home" size={28} color={color} />
+            <Octicons name="home-fill" size={24} color={color} />
           ) : (
             <Octicons name="home" size={24} color={color} />
           )
@@ -19,15 +24,24 @@ export default function TabsLayout() {
       }} />
 
       <Tabs.Screen name="saved" options={{
-        title: "Saved"
+        title: "Saved",
+        tabBarIcon: ({ color, focused }) => {
+          return focused ? (
+            <Octicons name="bookmark-filled" size={24} color={color} />
+          ) : (
+            <Octicons name="bookmark" size={24} color={color} />
+          )
+        },
       }} />
 
       <Tabs.Screen name="tasks" options={{
-        title: "Tasks"
+        title: "Tasks",
+        tabBarIcon: ({color}) => <Octicons name = "tasklist" size = {24} color={color}/>
       }} />
 
       <Tabs.Screen name="notes" options={{
-        title: "Notes"
+        title: "Notes",
+        tabBarIcon: ({color}) => <Octicons name = "paperclip" size = {24} color={color}/>
       }} />
     </Tabs>
   );
---
+++ constants/Colors.ts
@@ -0,0 +1,6 @@
+const Colors = {
+    primary: "#5a95f2", 
+    cardBackground: "#d9d9d9"
+};
+
+export default Colors;
