c472d7eea3cd6322aea0923aa89fb9aebc02364d
Vladimir Vasilev <vl.vasilev08@gmail.com>
2025-08-06 20:11:38 +0300

    com

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function SavedScreen(){
+    return (
+        <View>
+            <Text>
+                This is the saved page
+            </Text>
+        </View>
+    )
+}
